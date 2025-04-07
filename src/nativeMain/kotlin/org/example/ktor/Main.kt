7c4512ecd4cdb312a7cb4bf4a7f8adbdf160d70c
pm47 <pm.padiou@gmail.com>
2025-04-07 15:33:59 +0200

    optionally set caPath

--- src/nativeMain/kotlin/org/example/ktor/Main.kt
+++ src/nativeMain/kotlin/org/example/ktor/Main.kt
@@ -7,11 +7,17 @@ import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
 
 
-fun main() = runBlocking {
+fun main(args: Array<String>) = runBlocking {
 
     val client = HttpClient(Curl) {
-
+        args.firstOrNull()?.let { path ->
+            println("setting caPath=$path")
+            engine {
+                caPath = path
+            }
+        }
     }
+    
     val res = client.get("https://kotlinlang.org/")
     println("status=${res.status} bytes=${res.bodyAsBytes().size}")
 
